287d85427482d366667e6ce04c0202213270e5a5
Timur Kim <allrighty1809@gmail.com>
2021-05-15 11:45:23 +0600

    Move globalStyle from index to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import Footer from "./components/footer";
 import Header from "./components/header";
+import { GlobalStyle } from "./global/styles";
 
 function App({ className: c }) {
   return (
     <div className={c}>
+      <GlobalStyle />
       <Header />
       <Footer />
     </div>
